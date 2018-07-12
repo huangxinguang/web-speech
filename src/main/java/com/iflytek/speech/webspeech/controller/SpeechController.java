cef801223f2314ad7b10d66dce808a9e54c6df2d
hxguang <hxguang911219@163.com>
2018-07-12 15:45:28 +0800

    重构代码

--- src/main/java/com/iflytek/speech/webspeech/controller/SpeechController.java
+++ src/main/java/com/iflytek/speech/webspeech/controller/SpeechController.java
@@ -7,12 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Iterator;
 
 /**
  * @author xghuang
